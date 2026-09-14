205e9d7a723abcc6c36221ef03baf7ccb4cd1013
rowan <84088833+r0wanda@users.noreply.github.com>
2026-09-14 13:04:04 -0700

    gpod start

--- src/gpod/gpod.cpp
+++ src/gpod/gpod.cpp
@@ -1,5 +1,31 @@
 #include "gpod.hpp"
+#include <iostream>
+#include <
 
-GpodSong::GpodSong() {}
+GpodTrack::GpodTrack() {}
 
-Gpod::Gpod() {}
+Gpod::Gpod(std::string dbPath, std::function<void(GError*, bool)> errHandle):
+path(dbPath), errorHandle(errHandle), err(nullptr), tracks() {
+    itdb = itdb_parse(path, &err);
+	throwG(true);
+}
+
+void Gpod::process(std::function<void(int)> perCb) {
+	size_t trLen = g_list_length(itdb->tracks)
+	GList *it;
+	for (it = itdb->tracks; it != NULL; it = it->next) {
+		Itdb_Track *tr = (Itdb_Track*)it->data;
+		tracks.push_back(GpodTrack(tr));
+	}
+}
+
+void Gpod::throwG(bool fatal) {
+	if (!err) return;
+	if (err->message) errHandle(err, fatal);
+	g_error_free(err);
+	err = nullptr;
+}
+
+Gpod::~Gpod() {
+	itdb_free(itdb);
+}
--- src/gpod/gpod.hpp
+++ src/gpod/gpod.hpp
@@ -1,15 +1,30 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <GLib.h>
+#include <functional>
 #include <gpod/itdb.h>
 
-class GpodSong {
+class GpodTrack {
 public:
-	Itdb_Track track;
+	Itdb_Track *track;
 	std::string title;
-	GpodSong();
+	GpodTrack(Itdb_Track *tr);
+};
+class GpodAlbum {
+
 };
 
 class Gpod {
 public:
-	Gpod();
+	Itdb_iTunesDB *itdb;
+	std::vector<GpodTrack> tracks;
+	Gpod(std::string dbPath, std::function<void(GError*, bool)> errHandle);
+	void process(std::function<void(int)> perCb);
+	~Gpod();
+private:
+	void throwG(bool fatal);
+	std::string path;
+	std::function<void(GError*, bool)> errHandler;
+	GError *err;
 };
