72fc64ba7811e8ac567f73adc40d402426e197bd
rowan <84088833+r0wanda@users.noreply.github.com>
2026-09-15 13:04:04 -0700

    gpod stuff

--- src/gpod/gpod.cpp
+++ src/gpod/gpod.cpp
@@ -1,6 +1,6 @@
 #include "gpod.hpp"
 #include <iostream>
-#include <
+#include <thread>
 
 GpodTrack::GpodTrack() {}
 
@@ -11,11 +11,41 @@ path(dbPath), errorHandle(errHandle), err(nullptr), tracks() {
 }
 
 void Gpod::process(std::function<void(int)> perCb) {
-	size_t trLen = g_list_length(itdb->tracks)
+	int per = 0;
+	perCb(per);
+	size_t trLen = g_list_length(itdb->tracks);
 	GList *it;
 	for (it = itdb->tracks; it != NULL; it = it->next) {
 		Itdb_Track *tr = (Itdb_Track*)it->data;
-		tracks.push_back(GpodTrack(tr));
+		GpodTrack *gt = new GpodTrack(tr);
+		std::string arName(tr->artist);
+		std::string alName(tr->album);
+
+		// add to tracks
+		tracks.push_back(gt);
+
+		// find or create artist
+		GpodArtist *artist;
+		if (artists.find(arName) == artists.end()) {
+			artist = new GpodArtist(arName);
+			artists[arName] = artist;
+		} else {
+			artist = artists.at(arName);
+		}
+		artist->tracks.push_back(gt);
+		gt->artist = artist;
+
+		// add album to artist, synchronise everything
+		GpodAlbum *album;
+		if (artist->albums.find(alName) == artists->albums.end()) {
+			album = new GpodAlbum(alName);
+			artist->albums[alName] = album;
+		} else {
+			album = artist->albums.at(alName);
+		}
+		album->tracks.push_back(gt);
+		album->artist = artist;
+		gt->album = album;
 	}
 }
 
@@ -28,4 +58,13 @@ void Gpod::throwG(bool fatal) {
 
 Gpod::~Gpod() {
 	itdb_free(itdb);
+	for (auto &tr : tracks) {
+		delete tr;
+	}
+	for (auto &ar : artists) {
+		delete ar.second;
+	}
+	for (auto &al : albums) {
+		delete al.second;
+	}
 }
--- src/gpod/gpod.hpp
+++ src/gpod/gpod.hpp
@@ -1,24 +1,47 @@
 #pragma once
+#include <map>
 #include <string>
 #include <vector>
 #include <GLib.h>
 #include <functional>
 #include <gpod/itdb.h>
 
+class GpodArtist;
+class GpodAlbum;
+
 class GpodTrack {
 public:
 	Itdb_Track *track;
+	GpodArtist *artist;
+	GpodAlbum *album;
 	std::string title;
 	GpodTrack(Itdb_Track *tr);
 };
 class GpodAlbum {
-
+public:
+	GpodArtist *artist;
+	std::vector<GpodTrack*> tracks;
+	GpodAlbum(std::string n);
 };
+class GpodArtist {
+public:
+	std::map<std::string, GpodAlbum*> albums;
+	std::vector<GpodTrack*> tracks;
+	std::string name;
+	GpodArtist(std::string n);
+};
+class GpodPlaylist {
+public:
+
+	std::vector<GpodTrack*> tracks;
+}
 
 class Gpod {
 public:
 	Itdb_iTunesDB *itdb;
-	std::vector<GpodTrack> tracks;
+	std::vector<GpodTrack*> tracks;
+	std::map<std::string, GpodArtist*> artist;
+	std::map<std::string, GpodAlbum*> albums;
 	Gpod(std::string dbPath, std::function<void(GError*, bool)> errHandle);
 	void process(std::function<void(int)> perCb);
 	~Gpod();
